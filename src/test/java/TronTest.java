747307ce440860f17391a0a8858a8c7e41f450ed
TristanBrismontier <tristan.brismontier@gmail.com>
2015-08-26 22:19:58 +0200

    Add Junit

--- src/test/java/TronTest.java
+++ src/test/java/TronTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -17,4 +20,27 @@ public class TronTest {
         assertEquals(2300,tron.getGraph().getEdgeSize());
     }
 
+    @Test
+    public void restoreEdge(){
+        TronUniverse tron = new TronUniverse();
+        int startEdges = tron.getGraph().getEdgeSize();
+        List<Node> oppos = new ArrayList<>();
+        Node oldopponent=null;
+        Node opponent = tron.getGraph().getNodes().get(new Node(15, 10));
+        oppos.add(opponent);
+        for (int i = 0; i <20 ; i++) {
+            oldopponent = opponent;
+            tron.getGraph().addOpponent(opponent);
+            opponent = tron.getGraph().getDirection(opponent);
+            oppos.add(opponent);
+            if (oldopponent != null) {
+                tron.getGraph().removeEdge(opponent, oldopponent, false);
+            }
+        }
+        assertEquals(true,startEdges>tron.getGraph().getEdgeSize());
+        //Restore Edges of dead opponent
+        tron.getGraph().restorePath(oppos);
+        assertEquals(startEdges,tron.getGraph().getEdgeSize());
+
+    }
 }
